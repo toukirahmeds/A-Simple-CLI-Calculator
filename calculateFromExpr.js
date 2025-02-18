7858f255f02a0411993c78d9ddbd2aa8cbbdbc01
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2025-02-18 20:20:11 +0600

    getHasAcceptableChars and getIsSymmetric functions implemented

--- calculateFromExpr.js
+++ calculateFromExpr.js
@@ -1,3 +1,5 @@
+const ACCEPTED_CHARS = "0123456789+-*/()";
+
 /**
  * Calculates using PEMDAS rule.
  * 
@@ -5,7 +7,67 @@
  * @returns {number}
  */
 const calculateUsingPemdas = (parsedArr) => {
+    console.log(parsedArr);
+};
 
+/**
+ * Check if all the characters are acceptable.
+ * 
+ * @param {Array<string>} exprArr 
+ */
+const getHasAcceptableChars = (exprArr) => exprArr.every(
+    elem => ACCEPTED_CHARS.includes(elem)
+);
+
+/**
+ * Check if the parentheses are in symmetric.
+ * 
+ * @param {Array<string>} exprArr 
+ */
+const getIsSymmetric = (exprArr) => {
+    const stack = [];
+    let elem;
+
+    for (let i = 0; i < exprArr.length; i++) {
+        elem = exprArr[i];
+
+        if (elem === "(") {
+            stack.push(elem);
+        } else if (elem === ")") {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            if (stack[stack.length - 1] === "(") {
+                stack.pop();
+            }
+        }
+    }
+
+    if (stack.length > 0) {
+        return false;
+    }
+
+    return true;
+};
+
+/**
+ * Check if the expression is valid.
+ * 
+ * @param {Array<string>} exprArr 
+ */
+const checkIsValid = (exprArr) => {
+    if (!getHasAcceptableChars(exprArr)) {
+        console.error("Unacceptable characters.");
+        return false;
+    }
+
+    if (!getIsSymmetric(exprArr)) {
+        console.error("Expression is not symmetric.");
+        return false;
+    }
+
+    return true;
 };
 
 
@@ -15,7 +77,14 @@ const calculateUsingPemdas = (parsedArr) => {
  * @param {string} expr 
  */
 const calculateFromExpr = (expr) => {
-    console.log(expr);
+    const trimmedArr = expr.split("").filter(elem => elem.trim());
+    console.log(trimmedArr);
+    if (!checkIsValid(trimmedArr)) {
+        console.error("Invalid expression.");
+        process.exit();
+    }
+
+    // calculateUsingPemdas(parsedArr);
 };
 
 module.exports = calculateFromExpr;
