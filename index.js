76ef7817815a62732767f733deaf376757bfa255
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2025-02-19 17:30:50 +0600

    Implement '--help' argument feature

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const calculateFromCmdLineArgs = require("./calculateFromCmdLineArgs");
 const calculateFromConsoleInput = require("./calculateFromConsoleInput");
+const { printManualAndExit } = require("./utils");
 
 const commandLineExpr = process.argv[2];
 
 const main = async () => {
     let result;
+
+    if (commandLineExpr === "--help") {
+        printManualAndExit();
+    }
 
     if (commandLineExpr) {
         result = calculateFromCmdLineArgs(commandLineExpr);
